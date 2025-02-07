68a6b601225fe5147baaa71b1974fe50ba09899b
Fabian <50114802+fabalexsie@users.noreply.github.com>
2025-02-07 13:04:36 +0100

    added coil for image loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.coil.coil)
+    implementation(libs.coil.network)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
