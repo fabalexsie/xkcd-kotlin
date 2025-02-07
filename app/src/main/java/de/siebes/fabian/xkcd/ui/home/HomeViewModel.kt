61a21e8c78de70480df3aba558c0a9da5e84fb89
Fabian <50114802+fabalexsie@users.noreply.github.com>
2025-02-07 15:39:17 +0100

    basic home view setup (incl. ViewModel)

--- app/src/main/java/de/siebes/fabian/xkcd/ui/home/HomeViewModel.kt
+++ app/src/main/java/de/siebes/fabian/xkcd/ui/home/HomeViewModel.kt
@@ -3,11 +3,52 @@ package de.siebes.fabian.xkcd.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import androidx.lifecycle.viewModelScope
+import de.siebes.fabian.xkcd.helper.ComicLoader
+import de.siebes.fabian.xkcd.model.Comic
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _loading = MutableLiveData<Boolean>().apply {
+        value = false
     }
-    val text: LiveData<String> = _text
+
+    private val _comic = MutableLiveData<Comic>().apply {
+        value = null
+    }
+
+    private val _error = MutableLiveData<String?>()
+
+    val loading: LiveData<Boolean> = _loading
+
+    val error: LiveData<String?> = _error
+
+    fun loadComic(comicNumber: Int? = null) {
+        _error.value = null
+        _loading.value = true
+        viewModelScope.launch {
+                try {
+                    _comic.value = ComicLoader.loadComic(comicNumber)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    _error.value = e.message
+                } finally {
+                    _loading.value = false
+                }
+        }
+    }
+
+    val title: LiveData<String> = _comic.map { it?.title ?: "Loading..." }
+
+    val dateStr: LiveData<String> = _comic.map {
+        if (it == null) {
+            "Loading..." // TODO: use string resource
+        } else {
+            "${it.day}.${it.month}.${it.year}" // TODO: use string resource
+        }
+    }
+
+    val imgUrl: LiveData<String> = _comic.map { it?.imgUrl ?: "" }
 }
