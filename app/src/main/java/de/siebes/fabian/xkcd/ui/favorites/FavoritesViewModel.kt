b9449cd7f35657f539b7b661fbe936903f05bdcc
Fabian <50114802+fabalexsie@users.noreply.github.com>
2025-02-07 13:14:37 +0100

    rename notifications to favorites

--- app/src/main/java/de/siebes/fabian/xkcd/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/de/siebes/fabian/xkcd/ui/favorites/FavoritesViewModel.kt
@@ -1,13 +1,13 @@
-package de.siebes.fabian.xkcd.ui.notifications
+package de.siebes.fabian.xkcd.ui.favorites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FavoritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is favorites Fragment"
     }
     val text: LiveData<String> = _text
 }
